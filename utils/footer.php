495beb91577118edb7168c5805d878b7c267d324
dbalouch <92371010+dbalouch@users.noreply.github.com>
2023-09-11 01:49:02 -0700

    Add files via upload

---
+++ utils/footer.php
@@ -0,0 +1,31 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>E</strong><span class = "small footerSubtext">vent</span>
+                        <strong>M</strong><span class = "small footerSubtext">anagement</span>
+                        <strong>S</strong><span class = "small footerSubtext">ystem</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        Dhani Bux Avenue, Lashari, Co. Naudero
+                        &copy; Lashari Event Venue & Catering, 2023.
+                    </p>
+                </div>
+            </section>
+        
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
